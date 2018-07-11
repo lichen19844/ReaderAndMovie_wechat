7101c7126e153fe14f427c6c9e3b4d38070aaf0a
lichen <119449234@qq.com>
2018-07-11 13:48:53 +0800

    change pic

--- data/posts-data.js
+++ data/posts-data.js
@@ -8,7 +8,7 @@ var local_database = [
     content: "橘黄鞋郑菲，品尝求知味，看初花的地庐，楼外楼吃蟹，并列为秋天来杭州不能错过的事情",
     reading: "112",
     collection: "96",
-    headImgSrc: "/images/post/flower.gif",
+    headImgSrc: "/images/post/timg.gif",
     author: "林白衣",
     dateTime: "24小时前",
     detail: "所以每天跑步科不科学完全看自己，所以我建议大家为了自己的健康考虑，我们还是选择适当的跑步强度吧，不要太过执着，不要太过卖力，只有合适的跑步才能让你健康！",
@@ -16,7 +16,7 @@ var local_database = [
     music: {
       url: "http://music.163.com/song/media/outer/url?id=108219.mp3",
       title: "黄昏-周转雄",
-      coverImg: "http://img.zcool.cn/community/013e6d594cd1efa8012193a37f6066.gif"
+      coverImg: "http://5b0988e595225.cdn.sohucs.com/images/20171228/19f9301854ec4e1e98863342079fa86e.gif"
     }
   },
 
@@ -29,7 +29,7 @@ var local_database = [
     content: "大幅度水电费圣诞节快到了色即是空了积分看手机弗兰克是两房就是链接发生了丰富的似懂非懂",
     reading: "76",
     collection: "154",
-    headImgSrc: "/images/post/bee.gif",
+    headImgSrc: "/images/post/b1.jpg",
     author: "知更鸟",
     dateTime: "48天前",
     detail: "所以每天跑步科不科学完全看自己，所以我建议大家为了自己的健康考虑，我们还是选择适当的跑步强度吧，不要太过执着，不要太过卖力，只有合适的跑步才能让你健康！",
@@ -37,7 +37,7 @@ var local_database = [
     music: {
       url: "http://music.163.com/song/media/outer/url?id=108221.mp3",
       title: "女儿情-万晓利",
-      coverImg: "https://img5.duitang.com/uploads/item/201411/29/20141129013744_UJEuu.gif"
+      coverImg: "http://5b0988e595225.cdn.sohucs.com/images/20171228/19f9301854ec4e1e98863342079fa86e.gif"
     }
   },
 
@@ -64,12 +64,12 @@ var local_database = [
   {
     title: "微信·小程序开发工具安装指南",
     content: "这两天闲来无事，也安装了 “微信折叠”的开发工具来玩一下。以下是一些小道消息及使用体验，过两天我会写一篇文章以开发者的角度来详细评价微信小程序",
-    imgSrc: "/images/post/xiaolong.jpg",
+    imgSrc: "/images/post/xiaolong.png",
     reading: 102,
     detail: "这两天闲来无事，也安装了 “微信折叠”的开发工具来玩一下。以下是一些小道消息及使用体验，过两天我会写一篇文章以开发者的角度来详细评价微信小程序:微信小程序不能开发游戏类、直播类功能，小程序每个人关注的上限是20个（还不确定，不过我相信这是真的，这次公布的API里并没有视频组件。微信太大，苹果要有所顾忌，但是微信也要做出相应的让步)微信目前有没有同苹果商谈好，还是个未知数，毕竟会对AppStore有一定的冲击。抛弃了大量的javascript组件后，这个生态体系变得相当的封闭，微信解释肯定是：为了更好的性能提升。那么我们拭目以待。小程序的入口是微信里的三级菜单，就是在“Tab栏发现里的游戏下面加入一个“小程序”。反正，这一栏里的购物和游戏我是从来没点进去过的。以腾讯的尿性，小程序同服务号一样，其关系链及重要功能的开放程度会因“人”而异。对，优质的接口只会开放给腾讯的儿子们（滴滴呀、京东呀）",
     collection: 92,
     dateTime: "24小时前",
-    headImgSrc: "/images/post/smile.gif",
+    headImgSrc: "/images/post/b2.jpg",
     author: "猫是猫的猫",
     date: "Nov 20 2016",
     avatar: "/images/avatar/5.png",
@@ -89,7 +89,7 @@ var local_database = [
     detail: "消息，据国外媒体VentureBeat报道，一家名为Dexta Robotics的公司最近发布了一款有望变革虚拟现实手部追踪与交互方式的新产品。该产品名为“Dexmo”，它是一款像手套那样戴在手上使用的未来主义外骨骼。它内置大量的元件，能够与VR体验进行交互，可帮助你感觉握在你的双手的虚拟物体。Dexmo据Dexta称，“Dexmo是一款针对你的双手的机械外骨骼。它能够捕捉你的手部运动，以及提供即时的力反馈。有了Dexmo，你可以感受到虚拟物体的大小、形状和坚硬度。你可以接触数字世界。”市面上已经有数款产品旨在处理虚拟现实中的手部交互，也有相关的产品即将要进入市场。例如，颇受欢迎的HTC Vive头盔配有一副控制器，其控制器能够使得追踪系统看到你的双手，让你可以用它们来在特定体验中与物体进行交互。今年晚些时候，Oculus将开始出货类似的手部控制产品Oculus Touch。10月，索尼也将开始出货配备两个PlayStation Move手部控制器的PS VR。Leap Motion甚至更进一步：利用传感器来追踪手指和手部的运动。",
     collection: 26,
     dateTime: "24小时前",
-    headImgSrc: "/images/post/sun.gif",
+    headImgSrc: "/images/post/b3.jpg",
     author: "深白色",
     date: "Nov 20 2016",
     avatar: "../../../images/avatar/3.png",
@@ -104,12 +104,12 @@ var local_database = [
   {
     title: "爱奇艺创维开展战略合作，合力布局开放娱乐生态",
     content: "爱奇艺和创维分别作为国内领先的在线视频品牌",
-    imgSrc: "/images/iqiyi.png",
+    imgSrc: "/images/post/iqiyi.png",
     reading: 96,
     detail: "爱奇艺和创维分别作为国内领先的在线视频品牌和家电品牌。双方一直锐意创新，为用户提供优质的服务体验和产品体验。据悉，爱奇艺与创维将展开从资本到VIP会员服务等各方面的深入合作。籍由此次合作，爱奇艺将战略投资创维旗下拥有高端互联网电视品牌的酷开公司。从下一财年开始，创维旗下互联网电视将通过银河互联网电视集成播控平台，预置VIP会员服务及相关内容。这种捆绑终端与VIP内容的全新销售模式，将大幅提升互联网电视终端用户的体验，给予用户更多优质内容的选择。",
     collection: 26,
     dateTime: "21小时前",
-    headImgSrc: "/images/post/xinggan.gif",
+    headImgSrc: "/images/post/b4.jpg",
     author: "深白色",
     date: "Nov 20 2016",
     avatar: "../../../images/avatar/5.png",
