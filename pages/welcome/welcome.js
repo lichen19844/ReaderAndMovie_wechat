c0a428faf8d1bd60c497d64c796319f1832c943d
lichen <119449234@qq.com>
2018-07-12 11:41:27 +0800

    fix tabbar with switchTab

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,8 +1,9 @@
 Page({
   onTap: function(event){
     //navigateTo, redirectTo 只能打开非 tabBar 页面,即wx.navigateTo 和 wx.redirectTo 不允许跳转到 tabbar 页面
-    wx.navigateTo({
-      url:"../movies2/movies2"
+    // 跳转的 tabBar 页面的路径（需在 app.json 的 tabBar 字段定义的页面），路径后不能带参数,并关闭其他所有非 tabBar 页面
+    wx.switchTab({
+      url:"../posts/post"
     });
     // wx.redirectTo({
     //   url:"../posts/post"
