04d2ccd4345493696d7789d4319f08e9cde5b528
lichen <119449234@qq.com>
2018-07-12 11:28:27 +0800

    fix tabbar error

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,7 +1,8 @@
 Page({
   onTap: function(event){
+    //navigateTo, redirectTo 只能打开非 tabBar 页面,即wx.navigateTo 和 wx.redirectTo 不允许跳转到 tabbar 页面
     wx.navigateTo({
-      url:"../posts/post"
+      url:"../movies2/movies2"
     });
     // wx.redirectTo({
     //   url:"../posts/post"
