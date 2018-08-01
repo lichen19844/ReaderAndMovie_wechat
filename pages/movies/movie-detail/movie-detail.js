649ee02903818713bc58172047cddd610f5950ab
lichen <119449234@qq.com>
2018-08-02 07:31:25 +0800

    movie-detail data

--- pages/movies/movie-detail/movie-detail.js
+++ pages/movies/movie-detail/movie-detail.js
@@ -23,6 +23,41 @@ Page({
 
   processDoubanData: function(data){
     console.log(data);
+    //处理director数据，对绑定的movie变量填充，更新绑定变量
+    //先定义一个空的director变量，用来做判空处理
+    var director = {
+      avatar: "",
+      name: "",
+      id: "",
+    };
+    //directors[0]取自console测试得到的数据，if不写else的隐含意思是不满足条件就不做任何事
+    if(data.directors[0] != null) {
+      if(data.directors[0].avatar != null) {
+        director.avatar = data.directors[0].avatars.large
+      }
+      director.name = data.directors[0].name;
+      director.id = data.directors[0].id;
+    };
+    var movie = {
+      movieImg: data.images ? data.images.large: "",
+      country: data.couries[0],
+      title: data.title,
+      originalTitle: data.original_title,
+      wishCount: data.wish_count,
+      commentCount: data.comments_count,
+      year: data.year,
+      //join()函数的功能为：把数组的所有元素放入一个字符串，元素通过指定的分隔符分隔。join()连接数组，但是并不会改变数组本身
+      generes: data.genres.join("、"),
+      stars: utils.converToStarsArray(data.rating.stars),
+      score: data.rating.average,
+      //把处理过的对象director放入movies对象中
+      director: director,
+      casts: util.convertToCastString(data.casts),      
+      castsInfo: util.convertToCastInfos(data.casts),
+      summary: data.summary
+
+    };
+
   },
 
   /**
